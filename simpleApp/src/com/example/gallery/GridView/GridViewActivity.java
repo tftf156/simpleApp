e354e8db7f7c44946bb82fb4de09de0b3c275a82
Frank <tftf156@gmail.com>
2014-04-24 00:17:16 +0800

    可以選擇相片

--- simpleApp/src/com/example/gallery/GridView/GridViewActivity.java
+++ simpleApp/src/com/example/gallery/GridView/GridViewActivity.java
@@ -1,6 +1,7 @@
 package com.example.gallery.GridView;
 
 import java.io.File;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 import com.example.simpleapp.R;
@@ -11,6 +12,8 @@ import android.content.Intent;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -41,30 +44,98 @@ public class GridViewActivity extends Activity{
 		gridView.setAdapter(customGridAdapter);
 
 		gridView.setOnItemClickListener(new OnItemClickListener() {
+			/* (non-Javadoc)
+			 * @see android.widget.AdapterView.OnItemClickListener#onItemClick(android.widget.AdapterView, android.view.View, int, long)
+			 */
 			public void onItemClick(AdapterView<?> parent, View v,
 					int position, long id) {
-				Toast.makeText(GridViewActivity.this, position + "#Selected",
-						Toast.LENGTH_SHORT).show();
+				customGridAdapter.changeState(position, gridView);
 			}
 		});
 
 	}
 
-	private ArrayList<ImageItem> getData() {
+	private ArrayList<ImageItem> getData() 
+	{
 		final ArrayList<ImageItem> imageItems = new ArrayList<ImageItem>();
-		// retrieve String drawable array
 		File[] dirs = targetFile.listFiles();
+		
 		for (File file : dirs)
 		{
-			Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
-			bitmap = Bitmap.createScaledBitmap(bitmap, 45, 80, true);
-			bitmap = Bitmap.createScaledBitmap(bitmap, 90, 160, true);
-			
+			Bitmap bitmap = getBitmap(Uri.fromFile(file));
 			imageItems.add(new ImageItem(bitmap, file.getName()));
-			//bitmap.recycle();
 		}
-
+		
 		return imageItems;
-
 	}
+	
+	public Bitmap getBitmap(Uri uri) 
+	{
+        try {
+            InputStream in = getContentResolver().openInputStream(uri);
+            BitmapFactory.Options opts = new BitmapFactory.Options();
+            opts.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(in, null, opts);
+            in.close();
+ 
+            int sampleSize = computeSampleSize(opts, -1, 128 * 128);
+ 
+            in = getContentResolver().openInputStream(uri);
+            opts = new BitmapFactory.Options();
+            opts.inSampleSize = sampleSize;
+ 
+            Bitmap bmp = BitmapFactory.decodeStream(in, null, opts);
+            in.close();
+ 
+            return bmp;
+        } 
+        catch (Exception err) 
+        {
+            return null;
+        }
+    }
+ 
+    public static int computeSampleSize(BitmapFactory.Options options, 
+    		int minSideLength, int maxNumOfPixels) 
+    {
+ 
+        int initialSize = computeInitialSampleSize(options, minSideLength,
+                maxNumOfPixels);
+        int roundedSize;
+ 
+        if (initialSize <= 8) {
+            roundedSize = 1;
+            while (roundedSize < initialSize)
+            	roundedSize <<= 1;
+        }
+        else
+            roundedSize = (initialSize + 7) / 8 * 8;
+        
+        return roundedSize;
+    }
+ 
+    private static int computeInitialSampleSize(BitmapFactory.Options options,
+            int minSideLength, int maxNumOfPixels) {
+ 
+        double w = options.outWidth;
+        double h = options.outHeight;
+ 
+        int lowerBound = (maxNumOfPixels == -1) ? 1 : (int) Math.ceil(Math
+                .sqrt(w * h / maxNumOfPixels));
+        int upperBound = (minSideLength == -1) ? 128 : (int) Math.min(
+                Math.floor(w / minSideLength), Math.floor(h / minSideLength));
+ 
+        if (upperBound < lowerBound) 
+            // return the larger one when there is no overlapping zone.
+            return lowerBound;
+ 
+        if ((maxNumOfPixels == -1) && (minSideLength == -1)) 
+            return 1;
+        
+        else if (minSideLength == -1) 
+            return lowerBound;
+        
+        else 
+            return upperBound;
+    }
 }
--- simpleApp/src/com/example/gallery/GridView/GridViewAdapter.java
+++ simpleApp/src/com/example/gallery/GridView/GridViewAdapter.java
@@ -1,15 +1,24 @@
 package com.example.gallery.GridView;
 
 import java.util.ArrayList;
+import java.util.Vector;
 
 import com.example.simpleapp.R;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.Bitmap.Config;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,6 +27,7 @@ public class GridViewAdapter extends ArrayAdapter<ImageItem>{
 	private Context context;
 	private int layoutResourceId;
 	private ArrayList<ImageItem> data = new ArrayList<ImageItem>();
+	private Vector<Boolean> imageSelect = new Vector<>();
 
 	public GridViewAdapter(Context context, int layoutResourceId,
 			ArrayList<ImageItem> data) {
@@ -25,8 +35,28 @@ public class GridViewAdapter extends ArrayAdapter<ImageItem>{
 		this.layoutResourceId = layoutResourceId;
 		this.context = context;
 		this.data = data;
+		for(int i = 0 ; i < data.size(); i++)
+			imageSelect.add(false);
 	}
 
+	@Override 
+    public int getCount() {    
+            // TODO Auto-generated method stub    
+            return data.size();    
+    }    
+	
+	@Override
+	public ImageItem getItem(int position)
+	{
+		return data.get(position);
+	}
+ 
+    @Override 
+    public long getItemId(int position) {    
+            // TODO Auto-generated method stub    
+             return position;    
+    }
+	
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		View row = convertView;
@@ -45,9 +75,48 @@ public class GridViewAdapter extends ArrayAdapter<ImageItem>{
 
 		ImageItem item = data.get(position);
 		holder.imageTitle.setText(item.getTitle());
-		holder.image.setImageBitmap(item.getImage());
+		holder.image.setImageDrawable(makeBmp(position, imageSelect.elementAt(position)));
+		//holder.image.setImageBitmap(item.getImage());
 		return row;
 	}
+	
+	private LayerDrawable makeBmp(int id, boolean isChosen)
+	{
+		int width = data.get(id).getImage().getWidth() + 20;
+		int height = data.get(id).getImage().getHeight();
+		Bitmap spaceBitmap = Bitmap.createBitmap(width, height, Config.ARGB_8888);
+        // 根据isChosen来选取对勾的图片    
+        Bitmap seletedBmp;    
+        if(isChosen == true)    
+                seletedBmp = BitmapFactory.decodeResource(context.getResources(),    
+                                R.drawable.yes);    
+        else 
+                seletedBmp = BitmapFactory.decodeResource(context.getResources(),    
+                                R.drawable.no);    
+             
+        // 产生叠加图    
+        Drawable[] array = new Drawable[3];
+        array[0] = new BitmapDrawable(spaceBitmap);
+        array[1] = new BitmapDrawable(data.get(id).getImage());    
+        array[2] = new BitmapDrawable(seletedBmp);    
+        LayerDrawable la = new LayerDrawable(array);    
+        la.setLayerInset(0, 0, 0, 0, 0);    
+        la.setLayerInset(1, 20, 0, 0, 0);
+        la.setLayerInset(2, -10, -5, 40, 45); 
+                
+        return la;    //返回叠加后的图    
+	}
+	
+	public void changeState(int position, GridView gridView)
+	{    
+		imageSelect.setElementAt(!imageSelect.elementAt(position), position);
+		if(imageSelect.elementAt(position))
+			gridView.getChildAt(position).setBackgroundColor(Color.argb(128, 0, 171, 234));
+		else
+			gridView.getChildAt(position).setBackgroundColor(Color.argb(0, 0, 0, 0));
+		
+		notifyDataSetChanged();
+	 }
 
 	static class ViewHolder {
 		TextView imageTitle;
